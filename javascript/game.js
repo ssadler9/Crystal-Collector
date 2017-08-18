d79e07f2b970957c3597874aec614ae440af0fcd
Sam Sadler <samsadler9@sams-air.attlocal.net>
2017-08-18 18:12:05 -0500

    javascript is psuedo coded

---
+++ javascript/game.js
@@ -0,0 +1,9 @@
+// Have numbers to randomly generate 19 - 120
+// Display numbers in randomNumber div
+// Have random numbers assigned to crystal buttons 1-12
+// The buttons need to activate on click
+// Display crystal value to scoreTracker
+// values must add up to randomNumber
+// If scoreTracker = randomNumber user wins!
+// else user loses
+// tally wins and losses in gameStatus div
